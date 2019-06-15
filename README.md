df821697da1af7bff6768bd522a13b09ba6c0f24
IljaYuhtenko <ilja.yuhtenko@yandex.ru>
2019-06-15 17:00:03 +0300

    Implementation of the first task.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BinaryArithmeticApp
 An application to practice binary arithmetic in network related tasks made as a part of learning CCNA material
+
+# Supported tests
+For now, as a first step, it gives one task: to determine subnet ID, broadcast address, prefix length and, possibly, number of usable addresses.
+To ask for the last or not depends on the mask. The app will not ask that if the prefix is greater than 16.
+The randomly generated mask will be between 8 and 30.
---
+++ app.py
@@ -0,0 +1,36 @@
+import random
+import ipaddress
+
+subnet = ipaddress.IPv4Network((random.getrandbits(32), random.randint(8, 30)), strict=False)
+host = random.choice(list(subnet.hosts()))
+print("Find the subnet ID, broadcast address and prefix length for the following")
+print("IP address %s with mask %s" % (host, subnet.netmask))
+
+user_id = input("Enter subnet id: ")
+user_bcast = input("Enter broadcast address: ")
+user_plength = input("Enter prefix length: ")
+user_nhosts = subnet.num_addresses - 2
+if (subnet.prefixlen < 17):
+    user_nhosts = input("Enter tne number of usable addresses in the subnet: ")
+
+flag_id = user_id == str(subnet.network_address)
+flag_bcast = user_bcast == str(subnet.broadcast_address)
+flag_plength = int(user_plength) == subnet.prefixlen
+flag_nhosts = int(user_nhosts) == subnet.num_addresses - 2
+
+if flag_id and flag_bcast and flag_plength and flag_nhosts:
+    print("All the answers are correct! Good job!")
+    exit(0)
+
+if not flag_id:
+    print("You made a mistake identifying subnet ID.\nYour answer was %s, "
+          "while the correct is %s" % (user_id, str(subnet.network_address)))
+if not flag_bcast:
+    print("You made a mistake identifying broadcast address.\nYour answer was %s, "
+          "while the correct is %s" % (user_bcast, str(subnet.broadcast_address)))
+if not flag_plength:
+    print("You made a mistake identifying prefix length.\nYour answer was %s, "
+          "while the correct is %s" % (user_plength, subnet.prefixlen))
+if not flag_nhosts:
+    print("You made a mistake identifying the number of usable addresses in the subnet.\nYour answer was %s, "
+          "while the correct is %s" % (user_nhosts, subnet.num_addresses - 2))
